29ed32cb66307539506137da9a69abdc5b6a66b9
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-06-28 22:07:00 -0400

    Updates

---
+++ precache-manifest.d6750acbde263d6b27bff6628ca53fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf264ef5df825cbb312d7af814cf9f3",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "f945b8cfea4a0c6807fb",
+    "url": "/React-Clicky-Game/static/css/main.9607530c.chunk.css"
+  },
+  {
+    "revision": "6ce164e9c96b67147538",
+    "url": "/React-Clicky-Game/static/js/2.36e20b44.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/React-Clicky-Game/static/js/2.36e20b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f945b8cfea4a0c6807fb",
+    "url": "/React-Clicky-Game/static/js/main.ef345fc1.chunk.js"
+  },
+  {
+    "revision": "b4987c55b6aff42f3c37",
+    "url": "/React-Clicky-Game/static/js/runtime-main.0f69cd73.js"
+  },
+  {
+    "revision": "f801818f52dc7177f27bd689a0c28a2c",
+    "url": "/React-Clicky-Game/static/media/cityHall.f801818f.jpg"
+  }
+]);
